a29be8e24b5651d11b29a1c71c3d1137ef10dabc
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-01 20:02:54 +0000

    Update R__grant_access_new_users.sql

--- backend/src/main/resources/db/migration/R__grant_access_new_users.sql
+++ backend/src/main/resources/db/migration/R__grant_access_new_users.sql
@@ -2,11 +2,14 @@ CREATE ROLE read_user;
 CREATE ROLE write_user;
 CREATE ROLE admin_user;
 
-GRANT SELECT ON product, orders, order_product TO read_user;
-GRANT USAGE ON product, orders, order_product TO read_user;
+GRANT SELECT ON order_product TO read_user;
+GRANT SELECT ON orders TO read_user;
+GRANT SELECT ON product TO read_user;
 
-GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON product, orders, order_product TO write_user;
-GRANT USAGE, SELECT ON ALL SEQUENCES IN product, orders, order_product TO write_user;
+GRANT SELECT, UPDATE, INSERT ON order_product TO write_user;
+GRANT SELECT, UPDATE, INSERT ON orders TO write_user;
+GRANT SELECT, UPDATE, INSERT ON product TO write_user;
 
-GRANT ALL ON product, orders, order_product TO admin_user;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO admin_user;
+GRANT ALL ON order_product TO admin_user;
+GRANT ALL ON orders TO admin_user;
+GRANT ALL ON product TO admin_user;

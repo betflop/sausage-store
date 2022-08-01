e0185c0a20766dd19138984b7b4a7bdfd74ee203
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-01 10:04:41 +0000

    Add new file

---
+++ backend/src/main/resources/db/migration/R__grant_access_new_users.sql
@@ -0,0 +1,8 @@
+GRANT SELECT ON product, orders, order_product TO read_user;
+GRANT USAGE ON product, orders, order_product TO read_user;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON product, orders, order_product TO write_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN product, orders, order_product TO write_user;
+
+GRANT ALL ON product, orders, order_product TO admin_user;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO admin_user;

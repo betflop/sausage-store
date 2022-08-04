75f784fa82f3b7fd6804f090a85daf57d07f9d6c
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-04 17:53:35 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,8 +6,8 @@ set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 source .ENV_BACKEND
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+sudo rm -f .ENV_BACKEND || true
 sudo rm -f /home/jarservice/sausage-store.jar || true
-sudo rm -f /home/jarservice/.ENV_BACKEND || true
 
 #Скачиваем артефакт
 cd /tmp

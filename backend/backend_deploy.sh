b7972631733989616c36d0a4b6cf8ff3d0ea2fe4
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-16 14:39:54 +0000

    Update backend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
 set +e
+
 cat > .env <<EOF
 SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${SPRING_DATA_MONGODB_URI}
 EOF
+
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull ${CI_REGISTRY}/d.pashkov/sausage-store/sausage-backend:latest

4143c08e38fa03bf4e163cd565d39c2b59bd1eea
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-16 19:07:08 +0000

    Update backend_deploy.sh

--- backend/backend_deploy.sh
+++ backend/backend_deploy.sh
@@ -6,14 +6,13 @@ SPRING_DATASOURCE_URL=${SPRING_DATASOURCE_URL}
 SPRING_DATASOURCE_USERNAME=${SPRING_DATASOURCE_USERNAME}
 SPRING_DATASOURCE_PASSWORD=${SPRING_DATASOURCE_PASSWORD}
 SPRING_DATA_MONGODB_URI=${MONGO_DATA}
-#SPRING_DATA_MONGODB_URI=mongodb://sudmedru:Testusr1234@rc1b-k6kxnzzdzwda1z36.mdb.yandexcloud.net:27018/sudmedru?tls=true&replicaSet=rs01
 REPORT_PATH=/app/log/reports
 LOG_PATH=/app/log
 EOF
 
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-docker pull ${CI_REGISTRY}/d.pashkov/sausage-store/sausage-backend:latest
+docker pull ${CI_REGISTRY_IMAGE}/sausage-store/sausage-backend:latest
 docker stop backend || true
 docker rm backend || true
 
@@ -24,4 +23,4 @@ docker run -d --name backend \
     --pull always \
     --env-file .env \
     -p 8888:8888 \
-    ${CI_REGISTRY}/d.pashkov/sausage-store/sausage-backend:latest
+    ${CI_REGISTRY_IMAGE}/sausage-store/sausage-backend:latest

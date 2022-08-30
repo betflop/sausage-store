b75e47049811ec975dd4f7d15ea87480e79ef817
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-30 13:09:40 +0000

    Add backend-report_deploy.sh

---
+++ backend-report/backend-report_deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set +e
+cat > .env <<EOF
+VAULT_TOKEN=${VAULT_TOKEN}
+VAULT_URL=${VAULT_URL}
+REPORT_PATH=/app/log/reports
+LOG_PATH=/app/log
+EOF
+
+docker network create -d bridge sausage_network || true
+docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+docker pull $CI_REGISTRY_IMAGE/sausage-backend-report:latest
+docker stop backend-report || true
+docker rm backend-report || true
+
+set -e
+docker run -d --name backend-report \
+    --network=sausage_network \
+    --restart always \
+    --pull always \
+    --env-file .env \
+    $CI_REGISTRY_IMAGE/sausage-backend-report:latest

d4d2bdbbd045b99854b23ad70521d1bc55bb1df7
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-30 17:38:33 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ cat <<EOF | docker exec -i vault ash
   sleep 10;
   vault login ${VAULT_TOKEN}
   vault secrets enable -path=secret kv
-  vault kv put secret/sausage-store spring.datasource.username="${PSQL_USER}" spring.datasource.password="${PSQL_PASSWORD}" spring.datasource.url="${PSQL_DATASOURCE}" spring.data.mongodb.uri="${MONGO_URI}"
+  vault kv put secret/sausage-store spring.datasource.username="${PSQL_USER}" spring.datasource.password="${PSQL_PASSWORD}" spring.datasource.url="${PSQL_DATASOURCE}" spring.data.mongodb.uri="${MONGO_DATA}"
 EOF
 
 docker-compose up -d

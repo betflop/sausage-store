2aa86e02a7e441e80c8228b4fe61edc401302807
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-31 10:28:26 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-set -e
+set +e
+cat > .env.vault <<EOF
 VAULT_TOKEN=${VAULT_TOKEN}
 PSQL_DATASOURCE=${PSQL_DATASOURCE}
 PSQL_USER=${PSQL_USER}
 PSQL_PASSWORD=${PSQL_PASSWORD}
-MONGO_DATA=${MONGO_URI}
+MONGO_DATA=${MONGO_DATA}
+EOF
+
+set -e
+#VAULT_TOKEN=${VAULT_TOKEN}
+#PSQL_DATASOURCE=${PSQL_DATASOURCE}
+#PSQL_USER=${PSQL_USER}
+#PSQL_PASSWORD=${PSQL_PASSWORD}
+#MONGO_DATA=${MONGO_DATA}
 
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker-compose down --rmi local || true
-docker-compose up -d vault
+docker-compose up -d vault --env-file ./.env.vault
 
-cat <<EOF | sudo docker exec -i vault ash
+cat <<EOF | docker exec -i vault ash
   sleep 20;
   vault login ${VAULT_TOKEN};
   vault kv put secret/sausage-store spring.data.mongodb.uri='${MONGO_DATA}' spring.datasource.username="${PSQL_USER}" spring.datasource.password="${PSQL_PASSWORD}" spring.datasource.url="${PSQL_DATASOURCE}";

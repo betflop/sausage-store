ce0994ff961769d55c4ab43dd77d5d07ee46a7ac
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-31 10:57:28 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ VAULT_TOKEN=${VAULT_TOKEN}
 PSQL_DATASOURCE=${PSQL_DATASOURCE}
 PSQL_USER=${PSQL_USER}
 PSQL_PASSWORD=${PSQL_PASSWORD}
-MONGO_DATA=${MONGO_DATA}
+MONGO_VAULT=${MONGO_VAULT}
 EOF
 
 set -e
@@ -15,7 +15,7 @@ VAULT_TOKEN=${VAULT_TOKEN}
 PSQL_DATASOURCE=${PSQL_DATASOURCE}
 PSQL_USER=${PSQL_USER}
 PSQL_PASSWORD=${PSQL_PASSWORD}
-MONGO_DATA=${MONGO_DATA}
+MONGO_VAULT=${MONGO_VAULT}
 
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker-compose down --rmi local || true
@@ -24,7 +24,7 @@ docker-compose up -d vault
 cat <<EOF | docker exec -i vault ash
   sleep 10;
   vault login ${VAULT_TOKEN};
-  vault kv put secret/sausage-store spring.data.mongodb.uri="${MONGO_DATA}" spring.datasource.username="${PSQL_USER}" spring.datasource.password="${PSQL_PASSWORD}" spring.datasource.url="${PSQL_DATASOURCE}";
+  vault kv put secret/sausage-store spring.data.mongodb.uri="${MONGO_VAULT}" spring.datasource.username="${PSQL_USER}" spring.datasource.password="${PSQL_PASSWORD}" spring.datasource.url="${PSQL_DATASOURCE}";
 EOF
 
 docker-compose up -d

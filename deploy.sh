de1bfd0d0b62eae3a0a7bfafeeeef53099b8b074
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-08-31 09:14:18 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,7 @@ docker-compose up -d vault
 cat <<EOF | sudo docker exec -i vault ash
   sleep 20;
   vault login ${VAULT_TOKEN};
-  vault kv put secret/sausage-store spring.data.mongodb.uri="123" spring.datasource.username="${PSQL_USER}" spring.datasource.password="${PSQL_PASSWORD}" spring.datasource.url="${PSQL_DATASOURCE}";
+  vault kv put secret/sausage-store spring.data.mongodb.uri='mongodb://sudmedru:Testusr1234@rc1b-k6kxnzzdzwda1z36.mdb.yandexcloud.net:27018/sudmedru?tls=true&replicaSet=rs01' spring.datasource.username="${PSQL_USER}" spring.datasource.password="${PSQL_PASSWORD}" spring.datasource.url="${PSQL_DATASOURCE}";
 EOF
 
-echo ${MONGO_DATA}
 docker-compose up -d

d054453572d4764eeb7f791193aecb3209027e08
Дмитрий Пашков <sudmed-ru@yandex.ru>
2022-09-01 08:55:16 +0000

    Update backend-report_deploy.sh

--- backend-report/backend-report_deploy.sh
+++ backend-report/backend-report_deploy.sh
@@ -10,13 +10,5 @@ EOF
 docker network create -d bridge sausage_network || true
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker pull $CI_REGISTRY_IMAGE/sausage-backend-report:latest
-docker stop backend-report || true
-docker rm backend-report || true
-
-set -e
-docker run -d --name backend-report \
-    --network=sausage_network \
-    --restart always \
-    --pull always \
-    --env-file .env \
-    $CI_REGISTRY_IMAGE/sausage-backend-report:latest
+docker-compose pull
+docker-compose --env-file .env up -d backend-report
